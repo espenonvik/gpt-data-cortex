3e84b9c4c1ada1d8ba9313ed78de96fc9bb598ab
espenonvik <espenonvik.pedersen@technipfmc.com>
2023-10-03 17:45:49 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+
+import { OpenAI } from "langchain/llms/openai";
+import { config } from 'dotenv';
+import { ChatOpenAI } from "langchain/chat_models/openai";
+
+config();
+
+
+const llm = new OpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY, 
+  temperature: 0.9,
+});
+
+const chatModel = new ChatOpenAI();
+
+const text = "What would be a good company name for a company that makes colorful socks?";
+console.log(text);
+
+
+const llmResult = await llm.predict(text);
+console.log(llmResult);
+/*
+  "Feetful of Fun"
+*/
+
+const chatModelResult = await chatModel.predict(text);
+console.log(chatModelResult);
+/*
+  "Socks O'Color"
+*/
