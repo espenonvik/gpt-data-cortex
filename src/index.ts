01ab991f13324d2dde82d21f278ac13cde8ef678
espenonvik <espenonvik.pedersen@technipfmc.com>
2023-10-03 19:16:34 +0200

    Cleaning and chores

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@
 import {OpenAI} from "langchain/llms/openai";
 import {config} from 'dotenv';
 import {HumanMessage} from "langchain/schema";
-import { ChatOpenAI } from "langchain/chat_models/openai";
-import { TextLoader } from "langchain/document_loaders/fs/text";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
-import { RetrievalQAChain } from "langchain/chains";
+import {ChatOpenAI} from "langchain/chat_models/openai";
+import {TextLoader} from "langchain/document_loaders/fs/text";
+import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
+import {OpenAIEmbeddings} from "langchain/embeddings/openai";
+import {MemoryVectorStore} from "langchain/vectorstores/memory";
+import {RetrievalQAChain} from "langchain/chains";
 
 config();
 
@@ -21,9 +21,8 @@ const chatting = async () => {
     const chatModel = new ChatOpenAI();
 
     const query = "What would be a good company name for a company that makes colorful socks?";
-    console.log("Query", query);
-
     const messages = [new HumanMessage({content: query})];
+    console.log("Query", query);
 
     const llmResult = await llm.predictMessages(messages);
     console.log("LLM result", llmResult.content);
@@ -44,28 +43,25 @@ const chattingWithFile = async () => {
     });
 
     const splitDocs = await textSplitter.splitDocuments(data);
-
     const embeddings = new OpenAIEmbeddings();
-
     const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, embeddings);
-
-    const model = new ChatOpenAI({ modelName: "gpt-3.5-turbo" });
+    const model = new ChatOpenAI({modelName: "gpt-3.5-turbo"});
     const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
 
-    const response = await chain.call({
+    let response = await chain.call({
         query: "Who is the author of this story and what is the title?"
     });
     console.log(response);
 
-    const nextResponse = await chain.call({
+    response = await chain.call({
         query: "Can you summarize this story in a few sentences?"
     });
-    console.log(nextResponse);
+    console.log(response);
 
-    const lastResponse = await chain.call({
-        query: "Now tell me what would the typical audience of this story be?"
+    response = await chain.call({
+        query: "Tell me what would the typical audience of this story could be, and about which age? (keep it short)"
     });
-    console.log(lastResponse);
+    console.log(response);
 }
 
 await chattingWithFile();
